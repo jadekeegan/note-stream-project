abd31ed9914529b2d467166852053e337081d6dd
Ajay Gandecha <ajgandecha@gmail.com>
2025-10-03 15:14:59 -0400

    Initial Commit

---
+++ NoteStreamProject/NoteStreamProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteStreamProjectApp.swift
+//  NoteStreamProject
+//
+//  Created by Ajay Gandecha on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteStreamProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteStreamProjectTests/NoteStreamProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteStreamProjectTests.swift
+//  NoteStreamProjectTests
+//
+//  Created by Ajay Gandecha on 10/3/25.
+//
+
+import Testing
+@testable import NoteStreamProject
+
+struct NoteStreamProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
